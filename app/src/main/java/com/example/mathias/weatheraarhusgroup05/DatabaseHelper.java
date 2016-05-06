283a1a8caaf4020ef56ad00f96a21f10729d6fd7
mkm1192 <mathias@kraft-mathiasen.dk>
2016-05-06 12:07:53 +0200

    DB Helper started
    Started DP helper

--- app/src/main/java/com/example/mathias/weatheraarhusgroup05/DatabaseHelper.java
+++ app/src/main/java/com/example/mathias/weatheraarhusgroup05/DatabaseHelper.java
@@ -1,7 +1,56 @@
 package com.example.mathias.weatheraarhusgroup05;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.sql.Timestamp;
+
 /**
  * Created by Mathias on 06-05-2016.
  */
-public class DatabaseHelper {
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "MyDatabase.db";
+
+    private static final String TABLE_NAME = "tasks";
+    private static final String COLUMN_NAME_ID = "my_id";
+    private static final String COLUMN_NAME_DESC = "desc";
+    private static final String COLUMN_NAME_TEMP = "temp";
+    private static final String COLUMN_NAME_TIMESTAMP = "timestamp";
+
+    private static final String TEXT_TYPE = " TEXT";
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    COLUMN_NAME_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                    COLUMN_NAME_DESC + TEXT_TYPE + "," +
+                    COLUMN_NAME_TEMP + " INTEGER NOT NULL," +
+                    COLUMN_NAME_TIMESTAMP + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
+
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public int addWeatherInfo(WeatherInfo info) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME_DESC, info.getDescription());
+        values.put(COLUMN_NAME_TEMP, info.getTemp());
+
+        return (int) db.insert(TABLE_NAME, null, values);
+    }
 }
--- app/src/main/java/com/example/mathias/weatheraarhusgroup05/WeatherInfo.java
+++ app/src/main/java/com/example/mathias/weatheraarhusgroup05/WeatherInfo.java
@@ -1,7 +1,62 @@
 package com.example.mathias.weatheraarhusgroup05;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 /**
  * Created by Mathias on 06-05-2016.
  */
 public class WeatherInfo {
+    private int id;
+    private String description;
+    private int temp;
+    private Timestamp timestamp;
+
+    public WeatherInfo(int id, String description, int temp, Timestamp timestamp) {
+        this.id = id;
+        this.description = description;
+        this.temp = temp;
+        this.timestamp = timestamp;
+    }
+
+
+    public WeatherInfo(String description, int temp, Timestamp timestamp) {
+        this.description = description;
+        this.temp = temp;
+        this.timestamp = timestamp;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+
 }
