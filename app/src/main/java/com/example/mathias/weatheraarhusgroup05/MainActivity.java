1e945f322c4f7fc69d2902b9249cd45b96c5553b
jsaunte <jacob_saunte@hotmail.com>
2016-05-06 12:06:46 +0200

    maybe setup connection and started service

--- app/src/main/java/com/example/mathias/weatheraarhusgroup05/MainActivity.java
+++ app/src/main/java/com/example/mathias/weatheraarhusgroup05/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.mathias.weatheraarhusgroup05;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ServiceConnection weatherConnection;
+    private WeatherService wService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupAndBindWeatherService();
+    }
+
+    private void setupAndBindWeatherService() {
+        weatherConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                wService = ((WeatherService.WeatherBinder)service).getWeatherService();
+                Log.d("conn", "Weather service connected");
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                wService = null;
+                Log.d("diconn", "Weather service disconnected");
+            }
+        };
+        Intent weatherIntent = new Intent(MainActivity.this, WeatherService.class);
+        bindService(weatherIntent, weatherConnection, Context.BIND_AUTO_CREATE);
+        startService(weatherIntent);
     }
 }
--- app/src/main/java/com/example/mathias/weatheraarhusgroup05/WeatherService.java
+++ app/src/main/java/com/example/mathias/weatheraarhusgroup05/WeatherService.java
@@ -2,15 +2,29 @@ package com.example.mathias.weatheraarhusgroup05;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class WeatherService extends Service {
+    private final IBinder binder = new WeatherBinder();
+
     public WeatherService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service. hej hej
-        throw new UnsupportedOperationException("Not yet implemented");
+        return binder;
+    }
+
+    public class WeatherBinder extends Binder {
+        WeatherService getWeatherService() {
+            return WeatherService.this;
+        }
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
     }
 }
